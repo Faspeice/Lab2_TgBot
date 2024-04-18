1b1868ac3d8b708f41a5ec805b6946fc0624e7b9
faspeice <dima_kolodin@bk.ru>
2024-04-18 10:15:29 +0600

    examples plus some func

--- app/Bot.py
+++ app/Bot.py
@@ -1,25 +1,27 @@
+import asyncio
+import os
+import threading
 import requests
 from aiogram import F, Router
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.fsm.state import StatesGroup, State
 import app.keyboards as kb
 from aiogram.fsm.context import FSMContext
-from BotConfig import API_TOKEN
 import app.AlertsBase.bdrequests as rq
 from app.AlertsBase.bdrequests import messageAlerts, get_alerts, delete_my_alerts
-async def rasilka():
+from dotenv import load_dotenv
+load_dotenv()
+def rasilka():
     while 1:
-        await messageAlerts()
-
+        asyncio.run(messageAlerts())
+threading.Thread(target=rasilka).start()
 router = Router()
-
 def is_float(string):
   try:
     return float(string) and '.' in string  # True if string is a number contains a dot
   except ValueError:  # String is not a number
     return False
-
 
 class Crypto(StatesGroup):
     val1 = State()
@@ -29,41 +31,33 @@ class Alert(StatesGroup):
     val2 = State()
     costProcent = State()
     direction = State()
-
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await rq.set_user(message.from_user.id)
     await message.reply('Вас приветствует Erat бот! Я могу узнать курс криптовалют или установить alert. Что вам нужно сегодня?', reply_markup=kb.mai)
-@router.message(Command('ok'))
-async def get_help(message: Message):
-    await message.answer('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-
-@router.message(F.text == "Начать отслеживание")
-async def how_are_you(message: Message):
-    await  rasilka()
 @router.message(F.text == 'Чистка')
-async def reg_one(message: Message):
+async def chistka(message: Message):
     await delete_my_alerts(message.from_user.id)
     await message.answer('Alerts удалены ')
-@router.message(F.text == 'Узнать свои Alerts')
-async def reg_one(message: Message):
+@router.message(F.text == 'Проверить Alerts')
+async def proverka(message: Message):
     await get_alerts(message.from_user.id)
-@router.message(F.text == 'Узнать курс валюты')
-async def reg_one(message: Message,state: FSMContext):
+@router.message(F.text == 'Курс валюты')
+async def kurs_one(message: Message,state: FSMContext):
     await state.set_state(Crypto.val1)
     await message.answer('Введите название валюты')
 
 @router.message(Crypto.val1)
-async def reg_two(message: Message, state: FSMContext):
-    await state.update_data(val1 = message.text)
+async def kurs_two(message: Message, state: FSMContext):
+    await state.update_data(val1 = (message.text).upper())
     await state.set_state(Crypto.val2)
     await message.answer('Введите название валюты, в которой отобразится курс')
 @router.message(Crypto.val2)
-async def reg_three(message: Message, state: FSMContext):
-    await state.update_data(val2 = message.text)
+async def kurs_three(message: Message, state: FSMContext):
+    await state.update_data(val2 = (message.text).upper())
     data = await state.get_data()
     CRresponse = requests.get(
-        f"https://min-api.cryptocompare.com/data/pricemulti?fsyms={data["val1"]}&tsyms={data["val2"]}&api_key={API_TOKEN}")
+        f"https://min-api.cryptocompare.com/data/pricemulti?fsyms={data["val1"]}&tsyms={data["val2"]}&api_key={os.getenv('API_TOKEN')}")
     if "Response" in CRresponse.json():
         await message.answer("Что-то пошло не так, введите заново")
     else:
@@ -77,13 +71,13 @@ async def alert_one(message: Message, state: FSMContext):
 
 @router.message(Alert.val1)
 async def alert_two(message: Message, state: FSMContext):
-    await state.update_data(val1=message.text)
+    await state.update_data(val1=(message.text).upper())
     await state.set_state(Alert.val2)
     await message.answer('Введите название валюты, в которой будет использоваться курс')
 
 @router.message(Alert.val2)
 async def alert_three(message: Message, state: FSMContext):
-    await state.update_data(val2=message.text)
+    await state.update_data(val2=(message.text).upper())
     await state.set_state(Alert.costProcent)
     await message.answer('Введите процент')
 
@@ -91,21 +85,22 @@ async def alert_three(message: Message, state: FSMContext):
 async def alert_four(message: Message, state: FSMContext):
     await state.update_data(costProcent=message.text)
     await state.set_state(Alert.direction)
-    await message.answer('Выберите направление курса')##,reply_markup=kb.directi)
+    await message.answer('Выберите направление курса',reply_markup=kb.lowup)
+
 @router.message(Alert.direction)
 async def alert_five(message: Message, state: FSMContext):
-    await state.update_data(direction=message.text)
+    await state.update_data(direction=(message.text).lower())
     data = await state.get_data()
     CRresponse = requests.get(
-    f"https://min-api.cryptocompare.com/data/pricemulti?fsyms={data["val1"]}&tsyms={data["val2"]}&api_key={API_TOKEN}")
+    f"https://min-api.cryptocompare.com/data/pricemulti?fsyms={data["val1"]}&tsyms={data["val2"]}&api_key={os.getenv('API_TOKEN')}")
     if ("Response" in CRresponse.json() or (not(data["costProcent"].isdigit()) and not(is_float(data["costProcent"]))) or (data['direction']
-            != 'Вверх' and data['direction'] != 'Вниз')):
-        await message.answer("Что-то пошло не так, введите заново")
+            != 'вверх' and data['direction'] != 'вниз')):
+        await message.answer("Что-то пошло не так, введите заново",reply_markup=kb.mai)
     else:
         cost = str(CRresponse.json()[data["val1"]][data["val2"]])
         data["costProcent"] = str(float(cost) * (float(data["costProcent"]) / 100))
         await rq.setAlert(message.from_user.id,cost,data["costProcent"],data["val1"],data["val2"],data["direction"])
-        await message.answer("Alert успешно установлен!")
+        await message.answer(f"Alert в точке {cost} для {data['val1']} в {data['val2']} с направлением {data['direction']} успешно установлен!",reply_markup=kb.mai)
         await state.clear()
 
 
